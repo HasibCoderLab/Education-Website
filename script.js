da5fc4aa0f11067fe9abae637edb58d5a8653e29
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-01-25 14:22:19 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".onclick").style.display = "flex";
+
+})
+document.querySelector(".cancle").addEventListener("click", () => {
+    document.querySelector(".onclick").style.display = "none";
+
+})
